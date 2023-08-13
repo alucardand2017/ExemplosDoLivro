13229f32bfbd60edb6e7f14711dce884202ed2b6
anderson <alucardand2017@gmail.com>
2023-08-13 08:28:11 -0300

    Calcula o numero de e^x dando as opções do valor de x e calcular a quantidade de termos para precisao.

--- src/main/java/ExCap4Deitel.java
+++ src/main/java/ExCap4Deitel.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class ExCap4Deitel {
     public static void main(String[] args) {
-        int termos = perguntaAoUsuarioRetornandoInt("Digite o numero de termos para o calculo");
-        double termosDeEuler = termosDeEuler(termos);
-        imprimeResultadoNaTela("valor de e = ", termosDeEuler);
 
+        int valorX = perguntaAoUsuarioRetornandoInt("Digite o valor de x: ");
+        int termos = perguntaAoUsuarioRetornandoInt("Digite a quantidade de termos para calcular:  ");
+        double termosDeEuler = termosDeEuler(valorX, termos);
+
+        imprimeResultadoNaTela("valor de eˆx = ", termosDeEuler);
     }
-
     private static int fatorial(int nTermos) {
         int soma = 1;
         for(int i = 0; i < nTermos; i++){
@@ -15,20 +16,21 @@ public class ExCap4Deitel {
         }
         return soma;
     }
-    private static double termosDeEuler(int termos) {
+    private static double termosDeEuler(int x, int termos) {
         double soma = 0;
         for (int i = 0 ; i <= termos ; i++){
-            soma +=(1/(double)fatorial(i));
+            soma +=(Math.pow(x,i)/(double)fatorial(i));
         }
         return soma;
     }
     private static void imprimeResultadoNaTela(String frase, double termosDeEuler) {
+
         System.out.printf("%s %.10f",frase, termosDeEuler);
     }
     private static int perguntaAoUsuarioRetornandoInt(String digiteONumeroDeTermos) {
         int termos;
         Scanner entrada = new Scanner(System.in);
-        System.out.printf("%s :", digiteONumeroDeTermos);
+        System.out.printf("%s ", digiteONumeroDeTermos);
         termos = entrada.nextInt();
         return termos;
     }
